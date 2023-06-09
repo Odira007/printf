5bea4c5d514be8d834af41346b5ab8f9274ed872
psych0wh1z <ikeagudike@gmail.com>
2023-06-09 12:36:33 +0100

    Fix betty errors and rename file

--- _printf.c
+++ _printf.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
  * _printf - produces output according to a format
@@ -11,6 +9,7 @@ int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
+
 	va_start(args, format);
 
 	while (*format != '\0')
@@ -21,30 +20,31 @@ int _printf(const char *format, ...)
 			format++;
 
 			/* Handle conversion specifiers */
-			switch (*format) {
-				case 'c':
-				{
-					int c = va_arg(args, int);
-					fputc(c, stdout);
-					count++;
-					break;
-				}
-				case 's':
-				{
-					char *str = va_arg(args, char *);
-					fputs(str, stdout);
-					count += strlen(str);
-					break;
-				}
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				default:
-					putchar('%');
-					putchar(*format);
-					count += 2;
-					break;
+			switch (*format)
+			{
+			case 'c':
+			{
+				int c = va_arg(args, int);
+				fputc(c, stdout);
+				count++;
+				break;
+			}
+			case 's':
+			{
+				char *str = va_arg(args, char *);
+				fputs(str, stdout);
+				count += strlen(str);
+				break;
+			}
+			case '%':
+				putchar('%');
+				count++;
+				break;
+			default:
+				putchar('%');
+				putchar(*format);
+				count += 2;
+				break;
 			}
 		}
 		else
@@ -57,5 +57,5 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(args);
-	return count;
+	return (count);
 }
